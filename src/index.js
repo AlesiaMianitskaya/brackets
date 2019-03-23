2cf16b6f64f7f0a850f8a4a9e189e5dd2053119c
AlesiaMianitskaya <alesya.menitskaya@mail.ru>
2019-03-23 15:50:11 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let opened = [],
+    closed = [];
+
+  bracketsConfig.forEach(bracket => {
+    opened.push(bracket[0]);
+    closed.push(bracket[1]);
+  });
+
+  console.log("heheh  " + opened);
+  console.log("heheh  " + closed);
+
+  let matchingOpeningBracket, character;
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    character = str[i];
+
+    if (closed.indexOf(character) > -1) {
+      matchingOpeningBracket = opened[closed.indexOf(character)];
+      if (stack.length == 0 || stack.pop() != matchingOpeningBracket) {
+        return false;
+      }
+    } else {
+      stack.push(character);
+    }
+  }
+
+  return stack.length == 0;
+};
